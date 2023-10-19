db76b9b50ee0ff5c573c3c09470e2a9e89493765
DeveloperInside <enesm.hancer@gmail.com>
2023-10-19 22:33:25 +0300

    feat: some common styles added

--- src/theme/globalStyles.ts
+++ src/theme/globalStyles.ts
@@ -1,7 +1,20 @@
 import { StyleSheet } from 'react-native'
 
 export const styles = StyleSheet.create({
-  container: {
+  flexCenter: {
     flex: 1,
+    alignItems: 'center',
+  },
+  rowCenter: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  rowSpace: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  centerItems: {
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 })
