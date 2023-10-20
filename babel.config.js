7b6a8094dd43a1d43902d577f316f8c9c26abebc
DeveloperInside <enesm.hancer@gmail.com>
2023-10-21 00:20:27 +0300

    chore: babel module resolver configured

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,22 @@
+/* eslint-disable no-undef */
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-};
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./app'],
+        extensions: [
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+          '.json',
+          '.svg',
+          '.png',
+          '.jpg',
+        ],
+      },
+    ],
+  ],
+}
